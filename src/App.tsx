46e3cdd21bf56d012be0eaced5b3e8b0324bfc9b
박하은 <hani00415@naver.com>
2026-05-03 23:46:56 +0900

    [Feat] 태그 공통 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,18 @@
-import { ActionButton } from "./components/common/ActionButton";
+import { Tag } from "./components/common/Tag";
 
 function App() {
   return (
-    <div className="p-8 space-y-6 bg-white min-h-screen">
-      <h1 className="text-large-title text-main-500 mb-8">
-        Button Component Test
-      </h1>
-
-      {/* 1. 다양한 사이즈 테스트 */}
-      <div className="flex items-end gap-4">
-        <ActionButton size="xl" variant="darkGrey">
-          Button
-        </ActionButton>
-        <ActionButton size="lg" variant="darkGrey">
-          Button
-        </ActionButton>
-        <ActionButton size="md" variant="darkGrey">
-          Button
-        </ActionButton>
-        <ActionButton size="sm" variant="darkGrey">
-          Button
-        </ActionButton>
-      </div>
-
-      {/* 2. 다양한 색상 테스트 (오렌지 계열) */}
-      <div className="flex gap-4">
-        <ActionButton size="md" variant="orange">
-          Button
-        </ActionButton>
-        <ActionButton size="md" variant="mediumOrange">
-          Button
-        </ActionButton>
-        <ActionButton size="md" variant="lightOrange">
-          Button
-        </ActionButton>
-      </div>
-
-      {/* 3. 비활성화(Disabled) 테스트 & 아이콘 없는 버전 */}
-      <div className="flex gap-4">
-        <ActionButton size="lg" variant="orange" disabled>
-          Disabled Button
-        </ActionButton>
-        <ActionButton size="lg" variant="mediumGreen" showIcon={false}>
-          No Icon
-        </ActionButton>
-      </div>
+    <div className="flex gap-2 p-4">
+      <Tag variant="soft" color="orange" size="lg">
+        레이블
+      </Tag>
+      <Tag variant="outline" color="red" size="md">
+        레이블
+      </Tag>
+      <Tag variant="solid" color="blue" size="sm">
+        레이블
+      </Tag>
     </div>
   );
 }
-
 export default App;
