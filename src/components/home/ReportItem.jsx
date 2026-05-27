3cb5dfd9ce296f263f6c36d6521f739e7bedb21a
박하은 <hani00415@naver.com>
2026-05-27 16:01:07 +0900

    [Feat] ReportItem 컴포넌트 구현

---
+++ src/components/home/ReportItem.jsx
@@ -0,0 +1,33 @@
+import boyIcon from "../../assets/icons/test/boy.png";
+import chevronIcon from "../../assets/icons/common/chevron.svg";
+
+const ReportItem = ({ userInfo }) => {
+  return (
+    <div className="flex w-[338px] py-[12px] justify-between items-center bg-white cursor-pointer hover:opacity-80 transition-opacity">
+      <div className="flex items-center gap-[8px]">
+        <div className="flex w-[40px] h-[40px] justify-center items-center rounded-full bg-grey-50 overflow-hidden">
+          <img
+            src={boyIcon}
+            alt="프로필"
+            className="w-[22px] h-[22px] object-cover"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <span className="text-black text-e-subheadline">
+            {userInfo?.name || "이름"}
+          </span>
+          <div className="flex items-center mt-[2px] text-grey-700 text-footnote">
+            <span>{userInfo.date}</span>
+            <span className="mx-[2px]">・</span>
+            <span>{userInfo.testCount} 검사</span>
+          </div>
+        </div>
+      </div>
+
+      <img src={chevronIcon} alt="상세 이동" className="w-icon-sm h-icon-sm" />
+    </div>
+  );
+};
+
+export default ReportItem;
