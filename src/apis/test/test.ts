825b7f8ea2dee6fe0d37cee8fcdc4ab73ef8eab1
박하은 <hani00415@naver.com>
2026-05-31 21:14:27 +0900

    [Feat] /tests/{test_id}/image API 연동

--- src/apis/test/test.ts
+++ src/apis/test/test.ts
@@ -5,3 +5,19 @@ export const postTest = async (data: PostTestRequest) => {
   const response = await axiosInstance.post<PostTestResponse>("/tests", data);
   return response.data;
 };
+
+export const uploadTestImage = async (testId: number, file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await axiosInstance.post<string>(
+    `/tests/${testId}/image`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    },
+  );
+  return response.data;
+};
